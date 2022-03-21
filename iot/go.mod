cbce6b58acaceb6f0e9b9bd75426c57f0602716b
Joan Picornell <joan.picornell@gmail.com>
2022-03-21 15:25:43 +0100

    Added relay to control heater

--- iot/go.mod
+++ iot/go.mod
@@ -2,4 +2,7 @@ module github.com/jopicornell/thermonick
 
 go 1.15
 
-require github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+require (
+	github.com/warthog618/gpiod v0.8.0 // indirect
+	github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+)
--- iot/main.go
+++ iot/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"bytes"
+//	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/yryz/ds18b20"
 	"log"
-	"net/http"
+	"github.com/warthog618/gpiod"
+	"time"
+//	"net/http"
 )
 
 var sensorMap = map[string]string {
@@ -19,28 +21,45 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Printf("sensor IDs: %v\n", sensors)
 	var request []map[string]interface{}
-	for _, sensor := range sensors {
-		t, err := ds18b20.Temperature(sensor)
-		if err != nil {
-			log.Printf("Error getting temps: %s", err.Error())
-			continue
-		}
-		fmt.Printf("sensor: %s temperature: %.2f°C\n", sensor, t)
-		request = append(request, map[string]interface{}{
-			"temperature": t,
-			"type": sensorMap[sensor],
-		})
+
+	gpioHeater, err := gpiod.RequestLine("gpiochip0", 14, gpiod.AsOutput(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for _, sensor := range sensors {
+			t, err := ds18b20.Temperature(sensor)
+			if err != nil {
+				log.Printf("Error getting temps: %s", err.Error())
+				continue
+			}
+			fmt.Printf("sensor: %s temperature: %.2f°C\n", sensor, t)
+			request = append(request, map[string]interface{}{
+				"temperature": t,
+				"type": sensorMap[sensor],
+			})
+			if sensorMap[sensor] == "nick-busk" {
+				if (t < 21) {
+					fmt.Printf("Activating %s", sensorMap[sensor])
+					gpioHeater.SetValue(1)
+				} else {
+					fmt.Printf("Deactivating %s", sensorMap[sensor])
+					gpioHeater.SetValue(0)
+				}
+			}
+					}
+		time.Sleep(5 * time.Second)
 	}
 	body, err := json.Marshal(request)
 	if err != nil {
 		log.Fatal(err)
 	}
-	response, err := http.Post("https://umczz0pvpc.execute-api.eu-central-1.amazonaws.com/prod/temperatures", "application/json", bytes.NewBuffer(body))
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Created temperatures. Response: %+v", response)
+	log.Printf("tempObj: %+v", body)
+//	response, err := http.Post("https://umczz0pvpc.execute-api.eu-central-1.amazonaws.com/prod/temperatures", "application/json", bytes.NewBuffer(body))
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	log.Printf("Created temperatures. Response: %+v", response)
 }
