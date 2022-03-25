568fd2a1f4e305b22c0c2848c426eee1d1abb5c6
jopicornell <joan.picornell@gmail.com>
2022-03-25 10:15:49 +0100

    fixed bug sunset, sunrise

--- iot/utils/SunsetSunrise.go
+++ iot/utils/SunsetSunrise.go
@@ -11,9 +11,9 @@ var backupSunrise = time.Date(2009, time.November, 10, 8, 30, 0, 0, time.UTC)
 
 func SunsetSunrise(latitude float64, longitude float64, date time.Time) (sunrise time.Time, sunset time.Time) {
 	_, offset := date.Zone()
-	sunrise, sunset, err := sunrisesunset.GetSunriseSunset(latitude, longitude, float64(offset/int(time.Second.Seconds())), date)
+	sunrise, sunset, err := sunrisesunset.GetSunriseSunset(latitude, longitude, float64(offset/int(time.Hour.Seconds())), date)
 	if err != nil {
-		log.Logger.Errorf("Error: %+v Offset: %d", err, offset/int(time.Hour.Seconds()))
+		log.Logger.Errorf("Error: %+v", err)
 		sunrise = backupSunrise
 		sunset = backupSunset
 	}
